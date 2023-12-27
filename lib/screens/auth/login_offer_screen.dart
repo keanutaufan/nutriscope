4e6e3fe102cae27dcd5d5b8ecee16550a8eb89c9
keanutaufan <keanu.taufan21@gmail.com>
2023-12-27 11:22:41 +0700

    implement login offer screen

--- lib/screens/auth/login_offer_screen.dart
+++ lib/screens/auth/login_offer_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nutriscope/components/ns_fill_button.dart';
 import 'package:nutriscope/components/ns_ghost_button.dart';
 import 'package:nutriscope/components/ns_outline_button.dart';
+import 'package:nutriscope/screens/auth/auth_screen.dart';
 
 class LoginOfferScreen extends StatelessWidget {
   const LoginOfferScreen({super.key});
@@ -10,62 +11,80 @@ class LoginOfferScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Padding(
-        padding: const EdgeInsets.all(48),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Logo
-            Column(
-              children: [
-                Image.asset("assets/images/nutriscope-logo-text.png"),
-                const SizedBox(height: 16),
-                const Text(
-                  "Solusi digital informasi gizi dan alergen",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontFamily: "Montserrat",
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(48),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Logo
+              Column(
+                children: [
+                  Image.asset("assets/images/nutriscope-logo-text.png"),
+                  const SizedBox(height: 16),
+                  const Text(
+                    "Solusi digital informasi gizi dan alergen",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontFamily: "Montserrat",
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
+                ],
+              ),
 
-            // Buttons
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                // Register
-                NSFillButton(
-                  text: "Register",
-                  onPressed: () {},
-                  backgroundColor: const Color.fromRGBO(255, 215, 0, 1),
-                  foregroundColor: Colors.white,
-                ),
+              // Buttons
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Register
+                  NSFillButton(
+                    text: "Register",
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              const AuthScreen(initialIndex: 0),
+                        ),
+                      );
+                    },
+                    backgroundColor: const Color.fromRGBO(255, 215, 0, 1),
+                    foregroundColor: Colors.white,
+                  ),
 
-                const SizedBox(height: 16),
+                  const SizedBox(height: 16),
 
-                // Login
-                NSOutlineButton(
-                  text: "Login",
-                  onPressed: () {},
-                  outlineColor: const Color.fromRGBO(255, 215, 0, 1),
-                  foregroundColor: Colors.white,
-                ),
+                  // Login
+                  NSOutlineButton(
+                    text: "Login",
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              const AuthScreen(initialIndex: 1),
+                        ),
+                      );
+                    },
+                    outlineColor: const Color.fromRGBO(255, 215, 0, 1),
+                    foregroundColor: Colors.white,
+                  ),
 
-                const SizedBox(height: 16),
+                  const SizedBox(height: 16),
 
-                // Skip
-                NSGhostButton(
-                  text: "Lewati untuk sekarang",
-                  onPressed: () {},
-                  foregroundColor: Colors.grey,
-                ),
-              ],
-            )
-          ],
+                  // Skip
+                  NSGhostButton(
+                    text: "Lewati untuk sekarang",
+                    onPressed: () {},
+                    foregroundColor: Colors.grey,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
