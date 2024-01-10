7b5e32f9309787ec50cbad162e59d81b4b7803b5
keanutaufan <keanu.taufan21@gmail.com>
2024-01-10 20:49:37 +0700

    setup bottom navigation bar on app screen

--- lib/screens/app/app_screen.dart
+++ lib/screens/app/app_screen.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:nutriscope/screens/app/scanner_screen.dart';
 
-class AppScreen extends StatelessWidget {
+class AppScreen extends StatefulWidget {
   const AppScreen({super.key});
 
   @override
+  State<AppScreen> createState() => _AppScreenState();
+}
+
+class _AppScreenState extends State<AppScreen> {
+  int _selectedIndex = 1;
+
+  static const List<Widget> _pages = [
+    Text("This would be the ingredients screen"),
+    ScannerScreen(),
+    Text("This would be the account profile screen"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ScannerScreen(),
+    return Scaffold(
+      body: _pages.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard_customize),
+            label: "Atur",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner),
+            label: "Scan",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: "Akun",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
