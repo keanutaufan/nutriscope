e6a652b316573b57026c7f21d70a9febbbbdf7f3
keanutaufan <keanu.taufan21@gmail.com>
2024-01-10 20:37:41 +0700

    scanner ui

--- lib/screens/app/scanner_screen.dart
+++ lib/screens/app/scanner_screen.dart
@@ -44,26 +44,66 @@ class _ScannerScreenState extends State<ScannerScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // QR Scan
-        Expanded(
-          flex: 5,
-          child: QRView(
-            key: qrKey,
-            onQRViewCreated: _onQRViewCreated,
-          ),
-        ),
+    return SafeArea(
+      child: Container(
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          children: [
+            Container(
+              height: 72,
+              child: Center(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 72, vertical: 16),
+                  child: Image.asset("assets/images/nutriscope-logo-text.png"),
+                ),
+              ),
+            ),
 
-        // QR Result
-        Expanded(
-          child: Center(
-            child: (result != null)
-                ? Text("Data: ${result!.code}")
-                : const Text("Scan code"),
-          ),
+            // QR Scan
+            Expanded(
+                flex: 5,
+                child: Stack(
+                  children: [
+                    QRView(
+                      key: qrKey,
+                      overlay: QrScannerOverlayShape(),
+                      onQRViewCreated: _onQRViewCreated,
+                    ),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      padding: const EdgeInsets.only(bottom: 48),
+                      child: Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: const Color.fromRGBO(0, 0, 0, 0.3),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Text(
+                          "Arahkan Pemindai ke Kode QR",
+                          style: TextStyle(
+                            fontFamily: "Montserrat",
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+
+            // QR Result
+            Expanded(
+              child: Center(
+                child: (result != null)
+                    ? Text("Data: ${result!.code}")
+                    : const Text("Scan code"),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
