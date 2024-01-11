b87cdf91f78d0b25ba55aad1b851a6440cef63e4
keanutaufan <keanu.taufan21@gmail.com>
2024-01-11 10:30:08 +0700

    implement status switcher + model mock

--- lib/screens/app/ingredients_screen.dart
+++ lib/screens/app/ingredients_screen.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:nutriscope/components/ingredient_list_item.dart';
 
-class IngredientsScreen extends StatelessWidget {
+class IngredientsScreen extends StatefulWidget {
   const IngredientsScreen({super.key});
 
-  static const List<String> placeholder = [
-    "Tepung Terigu",
-    "Tepung Jagung",
-    "Tepung Ketan",
-    "Tepung Beras",
-    "Daging Ikan",
-    "Daging Ayam",
-    "Daging Sapi",
-    "Daging Babi",
-    "Daging Domba",
-    "Daging Kambing",
-    "Daging Kerbau",
-    "Minyak Nabati",
+  @override
+  State<IngredientsScreen> createState() => _IngredientsScreenState();
+}
+
+class _IngredientsScreenState extends State<IngredientsScreen> {
+  final List _placeholder = [
+    ["Tepung Terigu", 0],
+    ["Tepung Jagung", 0],
+    ["Tepung Ketan", 0],
+    ["Tepung Beras", 1],
+    ["Daging Ikan", 1],
+    ["Daging Ayam", 0],
+    ["Daging Sapi", 0],
+    ["Daging Babi", 0],
+    ["Daging Domba", 0],
+    ["Daging Kambing", 0],
+    ["Daging Kerbau", 0],
+    ["Minyak Nabati", 0],
   ];
+
+  void _setStatus(int index, int status) {
+    setState(() {
+      _placeholder[index][1] = status;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,11 +52,13 @@ class IngredientsScreen extends StatelessWidget {
           Expanded(
             child: ListView.builder(
               padding: const EdgeInsets.all(8),
-              itemCount: placeholder.length,
+              itemCount: _placeholder.length,
               itemBuilder: (context, index) {
                 return IngredientListItem(
-                  label: placeholder.elementAt(index),
-                  status: 0,
+                  label: _placeholder.elementAt(index)[0],
+                  status: _placeholder.elementAt(index)[1],
+                  onTapAllow: () => _setStatus(index, 0),
+                  onTapWarn: () => _setStatus(index, 1),
                 );
               },
             ),
