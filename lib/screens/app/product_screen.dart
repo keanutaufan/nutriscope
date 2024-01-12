70f9213e915e6291514fd88bb3ccb5379dad5daa
keanutaufan <keanu.taufan21@gmail.com>
2024-01-12 18:23:41 +0700

    fetch and render ingredient list

--- lib/screens/app/product_screen.dart
+++ lib/screens/app/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:nutriscope/components/ingredient_list_item.dart';
 import 'package:nutriscope/components/ns_ghost_button.dart';
 
 class ProductScreen extends StatefulWidget {
@@ -18,6 +19,7 @@ class _ProductScreenState extends State<ProductScreen> {
   bool loading = true;
   String name = "";
   String description = "";
+  List<dynamic> ingredients = [];
 
   @override
   void initState() {
@@ -31,6 +33,8 @@ class _ProductScreenState extends State<ProductScreen> {
         final data = doc.data() as Map<String, dynamic>;
         name = data["name"];
         description = data["description"];
+        ingredients = List.from(data["ingredients"]);
+        print(data);
       } else {
         exist = false;
       }
@@ -58,84 +62,110 @@ class _ProductScreenState extends State<ProductScreen> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    loading
-                        ? const Center(child: Text("Loading..."))
-                        : !exist
-                            ? const Center(
-                                child: Text("Barang Tidak Ditemukan!"),
-                              )
-                            : Column(
-                                children: [
-                                  const Text(
-                                    "Produk Terpindai!",
-                                    style: TextStyle(
-                                      fontFamily: "Montserrat",
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    textAlign: TextAlign.center,
+            Flexible(
+              fit: FlexFit.tight,
+              child: ListView(
+                padding: const EdgeInsets.all(32),
+                children: [
+                  loading
+                      ? const Center(child: Text("Loading..."))
+                      : !exist
+                          ? const Center(
+                              child: Text("Barang Tidak Ditemukan!"),
+                            )
+                          : Column(
+                              children: [
+                                const Text(
+                                  "Produk Terpindai!",
+                                  style: TextStyle(
+                                    fontFamily: "Montserrat",
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
                                   ),
+                                  textAlign: TextAlign.center,
+                                ),
 
-                                  // Image
-                                  SizedBox(
-                                    width: 150,
-                                    height: 150,
-                                    child: Image.network(
-                                      "https://placehold.co/120x100/png",
-                                      // width: 120,
-                                      // height: 100,
-                                    ),
+                                // Image
+                                SizedBox(
+                                  width: 150,
+                                  height: 150,
+                                  child: Image.network(
+                                    "https://placehold.co/120x100/png",
                                   ),
+                                ),
 
-                                  // Product Name
-                                  Text(
-                                    name,
-                                    style: const TextStyle(
-                                      fontFamily: "Montserrat",
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    textAlign: TextAlign.center,
+                                // Product Name
+                                Text(
+                                  name,
+                                  style: const TextStyle(
+                                    fontFamily: "Montserrat",
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
                                   ),
+                                  textAlign: TextAlign.center,
+                                ),
 
-                                  const SizedBox(height: 32),
+                                const SizedBox(height: 32),
 
-                                  const Text(
-                                    "Tentang Produk Ini:",
-                                    style: TextStyle(
-                                      fontFamily: "Montserrat",
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    textAlign: TextAlign.center,
+                                const Text(
+                                  "Tentang Produk Ini:",
+                                  style: TextStyle(
+                                    fontFamily: "Montserrat",
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
                                   ),
+                                  textAlign: TextAlign.center,
+                                ),
 
-                                  const SizedBox(height: 8),
+                                const SizedBox(height: 8),
 
-                                  // Description
-                                  Text(
-                                    description,
-                                    style: const TextStyle(
-                                      fontFamily: "Montserrat",
-                                      fontSize: 14,
-                                    ),
-                                    textAlign: TextAlign.justify,
-                                  )
-                                ],
-                              ),
-                    const SizedBox(height: 28),
-                    NSGhostButton(
-                      text: "Kembali ke Pemindai",
-                      onPressed: () => Navigator.of(context).pop(),
-                      foregroundColor: Colors.red,
-                    ),
-                  ],
-                ),
+                                // Description
+                                Text(
+                                  description,
+                                  style: const TextStyle(
+                                    fontFamily: "Montserrat",
+                                    fontSize: 14,
+                                  ),
+                                  textAlign: TextAlign.justify,
+                                ),
+
+                                const SizedBox(height: 32),
+
+                                const Text(
+                                  "Kandungan Produk:",
+                                  style: TextStyle(
+                                    fontFamily: "Montserrat",
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+
+                                const SizedBox(height: 16),
+
+                                // Ingredients
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  itemCount: ingredients.length,
+                                  itemBuilder: (context, index) {
+                                    return IngredientListItem(
+                                      id: ingredients[index],
+                                      label: ingredients[index],
+                                      status: 0,
+                                      onTapInfo: null,
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                  const SizedBox(height: 28),
+                  NSGhostButton(
+                    text: "Kembali ke Pemindai",
+                    onPressed: () => Navigator.of(context).pop(),
+                    foregroundColor: Colors.red,
+                  ),
+                ],
               ),
             ),
           ],
