27e744172a43955620c90671fe0fea4318179ab4
keanutaufan <keanu.taufan21@gmail.com>
2024-01-12 09:28:29 +0700

    add return to scanner button in product details screen

--- lib/screens/app/product_screen.dart
+++ lib/screens/app/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:nutriscope/components/ns_ghost_button.dart';
 
 class ProductScreen extends StatefulWidget {
   final String? qrPayload;
@@ -57,75 +58,86 @@ class _ProductScreenState extends State<ProductScreen> {
                 ),
               ),
             ),
-            loading
-                ? const Center(child: Text("Loading..."))
-                : !exist
-                    ? const Center(
-                        child: Text("Barang Tidak Ditemukan!"),
-                      )
-                    : Expanded(
-                        child: ListView(
-                          padding: const EdgeInsets.all(16),
-                          children: [
-                            // Heading
-                            const Text(
-                              "Produk Terpindai!",
-                              style: TextStyle(
-                                fontFamily: "Montserrat",
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    loading
+                        ? const Center(child: Text("Loading..."))
+                        : !exist
+                            ? const Center(
+                                child: Text("Barang Tidak Ditemukan!"),
+                              )
+                            : Column(
+                                children: [
+                                  const Text(
+                                    "Produk Terpindai!",
+                                    style: TextStyle(
+                                      fontFamily: "Montserrat",
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+
+                                  // Image
+                                  SizedBox(
+                                    width: 150,
+                                    height: 150,
+                                    child: Image.network(
+                                      "https://placehold.co/120x100/png",
+                                      // width: 120,
+                                      // height: 100,
+                                    ),
+                                  ),
+
+                                  // Product Name
+                                  Text(
+                                    name,
+                                    style: const TextStyle(
+                                      fontFamily: "Montserrat",
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+
+                                  const SizedBox(height: 32),
+
+                                  const Text(
+                                    "Tentang Produk Ini:",
+                                    style: TextStyle(
+                                      fontFamily: "Montserrat",
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+
+                                  const SizedBox(height: 8),
+
+                                  // Description
+                                  Text(
+                                    description,
+                                    style: const TextStyle(
+                                      fontFamily: "Montserrat",
+                                      fontSize: 14,
+                                    ),
+                                    textAlign: TextAlign.justify,
+                                  )
+                                ],
                               ),
-                              textAlign: TextAlign.center,
-                            ),
-
-                            // Image
-                            SizedBox(
-                              width: 150,
-                              height: 150,
-                              child: Image.network(
-                                "https://placehold.co/120x100/png",
-                                // width: 120,
-                                // height: 100,
-                              ),
-                            ),
-
-                            // Product Name
-                            Text(
-                              name,
-                              style: const TextStyle(
-                                fontFamily: "Montserrat",
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-
-                            const SizedBox(height: 32),
-
-                            const Text(
-                              "Tentang Produk Ini:",
-                              style: TextStyle(
-                                fontFamily: "Montserrat",
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-
-                            const SizedBox(height: 8),
-
-                            // Description
-                            Text(
-                              description,
-                              style: const TextStyle(
-                                fontFamily: "Montserrat",
-                                fontSize: 14,
-                              ),
-                              textAlign: TextAlign.justify,
-                            )
-                          ],
-                        ),
-                      ),
+                    const SizedBox(height: 28),
+                    NSGhostButton(
+                      text: "Kembali ke Pemindai",
+                      onPressed: () => Navigator.of(context).pop(),
+                      foregroundColor: Colors.red,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
